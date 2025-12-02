1f70e46627d1ae1bfcecfc7bb3fb23054589f1c2
IIIAMAH3 <yarikchamp0@gmail.com>
2025-12-02 20:12:20 +0100

    Add user functionality to add food

--- tracker/views.py
+++ tracker/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from .models import Food
+from .models import Food, Consume
+
 
 def index(request):
-    foods = Food.objects.all()
+    if request.method == "POST":
+        food_consumed = request.POST["food_consumed"]
+        consume = Food.objects.get(name=food_consumed)
+        user = request.user
+        consume_object = Consume(user=user, food_consumed=consume)
+        consume_object.save()
+        foods = Food.objects.all()
+
+    else:
+        foods = Food.objects.all()
     return render(request, "tracker/index.html", {"foods": foods})
